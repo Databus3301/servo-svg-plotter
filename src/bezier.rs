193ae34f37be4f56a668f4d1bf34b6121a2f9fc1
Databus3301 <felix@rose.ruhr>
2024-07-18 13:07:09 +0200

    add curvature impl

--- src/bezier.rs
+++ src/bezier.rs
@@ -54,4 +54,27 @@ impl Bezier {
             Bezier::Line(points) => points[2],
         }
     }
+
+    pub fn curvature_at_point(&self, t: f64) -> Result<Point, &'static str> {
+        if t > 1.0 || t < 0.0 {
+            return Err("t must be between 0 and 1");
+        }
+        match self {
+            Bezier::Cubic(points) => {
+                let x = 3.0 * (1.0 - t).powi(2) * (points[1].x - points[0].x) + 6.0 * (1.0 - t) * t * (points[2].x - points[1].x) + 3.0 * t.powi(2) * (points[3].x - points[2].x);
+                let y = 3.0 * (1.0 - t).powi(2) * (points[1].y - points[0].y) + 6.0 * (1.0 - t) * t * (points[2].y - points[1].y) + 3.0 * t.powi(2) * (points[3].y - points[2].y);
+                Ok(Point { x, y })
+            },
+            Bezier::Quadratic(points) => {
+                let x = 2.0 * (1.0 - t) * (points[1].x - points[0].x) + 2.0 * t * (points[2].x - points[1].x);
+                let y = 2.0 * (1.0 - t) * (points[1].y - points[0].y) + 2.0 * t * (points[2].y - points[1].y);
+                Ok(Point { x, y })
+            },
+            Bezier::Line(points) => {
+                let x = points[1].x - points[0].x;
+                let y = points[1].y - points[0].y;
+                Ok(Point { x, y })
+            },
+        }
+    }
 }
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -21,18 +21,27 @@ pub fn parse_svg(svg: Vec<String>) -> Vec<Bezier> {
     let mut state: ParseState = ParseState::Start;
     let mut cur_content = String::new();
     let mut start: Option<Point> = None;
-    let mut last_move = Point { x: 0.0, y: 0.0 };
+    let mut last_pos = Point { x: 0.0, y: 0.0 };
     let last_bezier: Bezier;
 
     let resolve_move = |content: &str| {
 
-        let mut split = content.split_whitespace();
+        let mut split = content.split_whitespace().peekable();
         let x = f64::from_str(split.next().unwrap()).unwrap();
         let y = f64::from_str(split.next().unwrap()).unwrap();
-        last_move = Point { x, y };
+        last_pos = Point { x, y };
 
         if start.is_none() {
-            start = Some(last_move);
+            start = Some(last_pos);
+        }
+
+        // handle implicit line syntax i.e -> m 1 1 2 2 3 3 z
+        if split.peek().is_some() {
+            let x = f64::from_str(split.next().unwrap()).unwrap();
+            let y = f64::from_str(split.next().unwrap()).unwrap();
+            last_pos = Point { x, y };
+            last_bezier = Bezier::new_l(start.unwrap(), last_pos, last_pos);
+            beziers.push(last_bezier);
         }
     };
 
@@ -46,7 +55,7 @@ pub fn parse_svg(svg: Vec<String>) -> Vec<Bezier> {
         let y2 = f64::from_str(split.next().unwrap()).unwrap();
         let x3 = f64::from_str(split.next().unwrap()).unwrap();
         let y3 = f64::from_str(split.next().unwrap()).unwrap();
-        let origin = last_move;
+        let origin = last_pos;
         last_bezier = Bezier::new_c(origin, Point { x: x0, y: y0 }, Point { x: x1, y: y1 }, Point { x: x2, y: y2 }, Point { x: x3, y: y3 });
         beziers.push(last_bezier);
     };
@@ -59,7 +68,7 @@ pub fn parse_svg(svg: Vec<String>) -> Vec<Bezier> {
         let y1 = f64::from_str(split.next().unwrap()).unwrap();
         let x2 = f64::from_str(split.next().unwrap()).unwrap();
         let y2 = f64::from_str(split.next().unwrap()).unwrap();
-        let origin = last_move;
+        let origin = last_pos;
         last_bezier = Bezier::new_q(origin, Point { x: x0, y: y0 }, Point { x: x1, y: y1 }, Point { x: x2, y: y2 });
         beziers.push(last_bezier);
     };
@@ -70,7 +79,7 @@ pub fn parse_svg(svg: Vec<String>) -> Vec<Bezier> {
         let y0 = f64::from_str(split.next().unwrap()).unwrap();
         let x1 = f64::from_str(split.next().unwrap()).unwrap();
         let y1 = f64::from_str(split.next().unwrap()).unwrap();
-        let origin = last_move;
+        let origin = last_pos;
         last_bezier = Bezier::new_l(origin, Point { x: x0, y: y0 }, Point { x: x1, y: y1 });
         beziers.push(last_bezier);
     };
