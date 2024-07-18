ef7a037016cc294f239cff2820fb7eabe6b79dd1
Databus3301 <felix@rose.ruhr>
2024-07-18 12:00:21 +0200

    parsing file

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,13 +1,20 @@
 use std::io::{BufRead, BufReader};
 use crate::bezier::Bezier;
 
-pub fn read_in(path: &str) -> Vec<&str> {
+pub fn read_in(path: &str) -> Vec<String> {
     let br = BufReader::new(std::fs::File::open(path).unwrap());
-    br.lines().filter(|&l| *l.unwrap().contains("d=\"")).collect()
+    br.lines()
+        .filter_map(|line_result| {
+            line_result.ok().and_then(|line| {
+                if line.contains("d=\"") { Some(line) } else { None }
+            })
+        })
+        .collect()
 }
 
 pub fn parse_svg(svg: Vec<&str>) -> Vec<Bezier> {
-    svg.iter().map(|l| {
-        l.split(" ")
-    })
+    //svg.iter().map(|l| {
+    //    l.split(" ")
+    //})
+    vec!()
 }
